2c320bd3f00be6110c5e2dcc30aa5bfbc6d2c93e
Benjamin Auquite <halomastar@gmail.com>
2025-12-09 22:57:20 -0600

    fix: add bounds check in updateParams to prevent IndexOutOfBoundsException when redo is true

--- src/main/java/com/kotor/resource/formats/ncs/utils/SubroutineState.java
+++ src/main/java/com/kotor/resource/formats/ncs/utils/SubroutineState.java
@@ -222,6 +222,13 @@ public class SubroutineState {
          }
       }
 
+      // Ensure this.params has enough elements when redo is true
+      if (redo && this.params.size() < types.size()) {
+         while (this.params.size() < types.size()) {
+            this.params.add(new Type((byte)-1));
+         }
+      }
+
       for (int i = 0; i < types.size(); i++) {
          Type newtype = types.get(i);
 
