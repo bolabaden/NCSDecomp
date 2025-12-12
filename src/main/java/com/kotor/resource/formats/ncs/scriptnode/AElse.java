0c04ade39fc54e904d5ccb0b7679bbcb0f6fae81
Benjamin Auquite <halomastar@gmail.com>
2025-12-11 18:18:03 -0600

    fix: output else if statements correctly when AElse contains AIf

--- src/main/java/com/kotor/resource/formats/ncs/scriptnode/AElse.java
+++ src/main/java/com/kotor/resource/formats/ncs/scriptnode/AElse.java
@@ -12,13 +12,38 @@ public class AElse extends ScriptRootNode {
    @Override
    public String toString() {
       StringBuffer buff = new StringBuffer();
-      buff.append(this.tabs + "else {" + this.newline);
+      
+      // Handle "else if" case: if the first (and only) child is an AIf, output "else if" instead of "else { if ... }"
+      if (this.children.size() == 1 && AIf.class.isInstance(this.children.get(0))) {
+         AIf ifChild = (AIf) this.children.get(0);
+         // Format condition similar to AControlLoop.formattedCondition()
+         String cond;
+         if (ifChild.condition() == null) {
+            cond = " ()";
+         } else {
+            String condStr = ifChild.condition().toString().trim();
+            boolean wrapped = condStr.startsWith("(") && condStr.endsWith(")");
+            cond = wrapped ? condStr : "(" + condStr + ")";
+            cond = " " + cond;
+         }
+         buff.append(this.tabs + "else if" + cond + " {" + this.newline);
+         
+         for (int i = 0; i < ifChild.children.size(); i++) {
+            buff.append(ifChild.children.get(i).toString());
+         }
+         
+         buff.append(this.tabs + "}" + this.newline);
+      } else {
+         // Standard else block
+         buff.append(this.tabs + "else {" + this.newline);
 
-      for (int i = 0; i < this.children.size(); i++) {
-         buff.append(this.children.get(i).toString());
+         for (int i = 0; i < this.children.size(); i++) {
+            buff.append(this.children.get(i).toString());
+         }
+
+         buff.append(this.tabs + "}" + this.newline);
       }
-
-      buff.append(this.tabs + "}" + this.newline);
+      
       return buff.toString();
    }
 }
